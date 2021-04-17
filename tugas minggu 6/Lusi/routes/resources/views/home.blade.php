8ab23f4951525772968a6cb5fb525ee6a3260afc
Lusiwatul <82276355+Lusiwatul@users.noreply.github.com>
2021-04-17 10:14:00 +0700

    tugas minggu 6

---
+++ tugas minggu 6/Lusi/routes/resources/views/home.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_','-', app()->getlocale())}}">
+<head>
+   <meta charset="utf-8">
+   <meta name="viewport" content="width-device-widh, initial- scale1.0">
+   <meta http-equiv="X-UA-Compitable" contet="ie=edge">
+   <title>{{ config('app.name')}}</title>
+</head>
+<body>
+   <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+              <h1 class="display-4">Halaman Home</h1>
+              <p class="lead">Halaman ini merupakan halaman home</p>
+        </div>
+    </div>
+</body>
+</html>
